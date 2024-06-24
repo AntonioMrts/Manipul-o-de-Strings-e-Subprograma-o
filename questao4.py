b67bb80261661a30a8e4263352925c04a29525b0
Antonio Junior <111703609+antonio-junior34@users.noreply.github.com>
2024-06-24 09:46:52 -0300

    Create questao4.py

---
+++ questao4.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que lê uma frase e substitui todas as ocorrências de espaço por "#".
+'''
+
+frase = input("Digite uma frase: ")
+substituir = frase.replace(" ", "#")
+
+print(f"Frase sem espaços: {substituir}")

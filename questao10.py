a939e13e8eb7fd386ea9acd31042c273db7c09eb
Antonio Junior <111703609+antonio-junior34@users.noreply.github.com>
2024-06-24 09:56:03 -0300

    Create questao10.py

---
+++ questao10.py
@@ -0,0 +1,8 @@
+data_nascimento = input("Digite sua data de nascimento (dd/mm/aaaa): ")
+
+dia, mes, ano = data_nascimento.split("/")
+
+meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", 
+         "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+
+print(f"Você nasceu em {dia} de {meses[int(mes) - 1]} de {ano}")

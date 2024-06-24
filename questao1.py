b15b967ac0c376198ce98e1ed91abbebccd5e8fa
Antonio Junior <111703609+antonio-junior34@users.noreply.github.com>
2024-06-24 09:40:46 -0300

    Create questao1.py

---
+++ questao1.py
@@ -0,0 +1,11 @@
+'''
+Escreva um programa que lê uma frase e uma String antiga e uma String nova. O programa deve
+imprimir uma String contendo a frase original, mas com a última ocorrência da String antiga
+substituída pela String nova.
+Exemplo:
+Frase: "Quem parte e reparte fica com a maior parte"
+String antiga: "parte"
+String nova: "parcela"
+Resultado a ser impresso no programa principal: "Quem parte e reparte
+fica com a maior parcela"
+'''

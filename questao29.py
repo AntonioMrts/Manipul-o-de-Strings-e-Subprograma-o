1428715e3e8da3d6282835cacb4371ae0963cc9e
Antonio Junior <111703609+antonio-junior34@users.noreply.github.com>
2024-06-24 11:17:39 -0300

    Create questao29.py

---
+++ questao29.py
@@ -0,0 +1,7 @@
+def somar_numero(num1, num2):
+    return num1 + num2
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+somar = somar_numero(n1, n2)
+print(f"A soma de {n1} + {n2} é {somar}")
